5c31a7f2c01d730d7518c5eee8950e762bc8e2c0
AlexDolz <aleksejdolznikov3@gmail.com>
2023-07-11 17:42:29 +0200

    added

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
 console.log(checkCoupon('123', '123', 'September 5, 2014', 'October 1, 2014')); // true
 console.log(checkCoupon('123a', '123', 'September 5, 2014', 'October 1, 2014')); // false
 
-// *********************************************************************************
+// ***************************Nr 1*************************************
 
 // Complete the function which converts hex number (given as a string) to a decimal number.
 
@@ -31,7 +31,7 @@ console.log(hexToDec('1')); // 1
 console.log(hexToDec('a')); // 10
 console.log(hexToDec('10')); // 16
 
-// *********************************************************************************
+// **********************************Nr2**********************************
 
 // I have a cat and a dog.
 
@@ -71,7 +71,7 @@ console.log(humanYearsCatYearsDogYears(1)); // [1, 15, 15]
 console.log(humanYearsCatYearsDogYears(2)); // [2, 24, 24]
 console.log(humanYearsCatYearsDogYears(10)); // [10, 56, 64]
 
-// *********************************************************************************
+// *****************************Nr3**************************************
 
 // Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
 
@@ -116,3 +116,65 @@ console.log(
   warnTheSheeps(['sheep', 'wolf', 'sheep', 'sheep', 'sheep', 'sheep', 'sheep'])
 ); //"Oi! Sheep number 5! You are about to be eaten by a wolf!"
 console.log(warnTheSheeps(['sheep', 'sheep', 'wolf'])); //"Pls go away and stop eating my sheep"
+
+// ***************************Nr 4***********************************
+
+// The Story:
+// Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+// Task Overview:
+// You have to write a function that accepts three parameters:
+
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+const enough = (cap, on, wait) => {
+  if (on + wait <= cap) {
+    return 0;
+  } else {
+    return Math.abs(cap - (on + wait));
+  }
+};
+
+console.log(enough(10, 5, 5)); // 0
+console.log(enough(100, 60, 50)); // 10
+console.log(enough(20, 5, 5)); // 0
+console.log(enough(84, 62, 69)); // 0
+
+// ******************************* Nr 5 ********************************
+
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+const capitalize = s => {
+  const arrayWord = s.split('');
+  const odd = arrayWord
+    .map((elem, i) => {
+      if (i % 2 === 0) {
+        return elem.toUpperCase();
+      } else {
+        return elem;
+      }
+    })
+    .join('');
+
+  const even = arrayWord
+    .map((elem, i) => {
+      if (i % 2 !== 0) {
+        return elem.toUpperCase();
+      } else {
+        return elem;
+      }
+    })
+    .join('');
+  return [odd, even];
+};
+
+console.log(capitalize('abcdef')); // ['AbCdEf', 'aBcDeF']
+console.log(capitalize('codewars')); // ['CoDeWaRs', 'cOdEwArS']
+console.log(capitalize('codewarriors')); // ['CoDeWaRrIoRs', 'cOdEwArRiOrS']

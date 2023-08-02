a67d9f45264c1fa010e76918626aefd06bc8757d
AlexDolz <aleksejdolznikov3@gmail.com>
2023-08-02 17:46:42 +0200

    updated practice

--- script.js
+++ script.js
@@ -401,3 +401,45 @@ console.log(solve('code'));
 console.log(solve('CODe'));
 console.log(solve('COde'));
 console.log(solve('Code'));
+
+// *************************** Nr 14 *****************************************
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+const addBinary = (a, b) => {
+  return (a + b).toString(2);
+};
+
+console.log(addBinary(1, 1));
+console.log(addBinary(5, 9));
+
+// ******************************* Nr 15 *************************************
+
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+const sortArray = array => {
+  const odd = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+  let oddIndex = 0;
+  return array.map(num => (num % 2 !== 0 ? odd[oddIndex++] : num));
+};
+
+// 2 solution
+// function sortArray(array) {
+//   let odd = array.filter(elem => elem % 2 !== 0).sort((a, b) => a - b);
+//   return array.map(elem => (elem % 2 === 0 ? elem : odd.shift()));
+// }
+
+console.log(sortArray([7, 1]));
+console.log(sortArray([5, 8, 6, 3, 4]));
+console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));

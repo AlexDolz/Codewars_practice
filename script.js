9b5bd5b3e5ca54d2416e5bb17c2cd29ae9c4fd4b
AlexDolz <aleksejdolznikov3@gmail.com>
2023-12-05 16:28:51 +0100

    updated

--- script.js
+++ script.js
@@ -584,3 +584,17 @@ function removeSmallest(numbers) {
 console.log(removeSmallest([1, 2, 3, 4, 5]));
 console.log(removeSmallest([5, 3, 2, 1, 4]));
 console.log(removeSmallest([2, 2, 1, 2, 1]));
+
+// ********************* Nr 20 ******************************
+
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+}
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'));
+console.log(
+  sayHello(['Franklin', 'Delano', 'Roosevelt'], 'Chicago', 'Illinois')
+);
+console.log(sayHello(['Wallace', 'Russel', 'Osbourne'], 'Albany', 'New York'));

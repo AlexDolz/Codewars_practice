56414156ce3336f403712febf7c9179681fb5e26
AlexDolz <aleksejdolznikov3@gmail.com>
2025-01-14 15:13:11 +0100

    added

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
 console.log(checkCoupon('123', '123', 'September 5, 2014', 'October 1, 2014')); // true
 console.log(checkCoupon('123a', '123', 'September 5, 2014', 'October 1, 2014')); // false
 
-// ***************************Nr 1*************************************
+// ***************************Nr 1*******************************
 
 // Complete the function which converts hex number (given as a string) to a decimal number.
 
@@ -683,3 +683,108 @@ function animal(obj) {
 console.log(animal({ name: 'dog', legs: 4, color: 'white' }));
 console.log(animal({ name: 'cock', legs: 2, color: 'red' }));
 console.log(animal({ name: 'rabbit', legs: 4, color: 'gray' }));
+
+// ************************** Nr 27 *****************************
+
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+const haystack_1 = [
+  '3',
+  '123124234',
+  undefined,
+  'needle',
+  'world',
+  'hay',
+  2,
+  '3',
+  true,
+  false,
+];
+const haystack_2 = [
+  '283497238987234',
+  'a dog',
+  'a cat',
+  'some random junk',
+  'a piece of hay',
+  'needle',
+  'something somebody lost a while ago',
+];
+const haystack_3 = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  8,
+  7,
+  5,
+  4,
+  3,
+  4,
+  5,
+  6,
+  67,
+  5,
+  5,
+  3,
+  3,
+  4,
+  2,
+  34,
+  234,
+  23,
+  4,
+  234,
+  324,
+  324,
+  'needle',
+  1,
+  2,
+  3,
+  4,
+  5,
+  5,
+  6,
+  5,
+  4,
+  32,
+  3,
+  45,
+  54,
+];
+const haystack_4 = [
+  'hay',
+  'junk',
+  'hay',
+  'hay',
+  'moreJunk',
+  'needle',
+  'randomJunk',
+];
+
+// function findNeedle(haystack) {
+//   const index = haystack.indexOf('needle');
+
+//   if (index !== -1) {
+//     return `found the needle at position ${index}`;
+//   } else {
+//     return `there is no needle in haystack`;
+//   }
+// }
+
+const findNeedle = haystack =>
+  `Found the needle at position ${haystack.indexOf('needle')}`;
+
+console.log(findNeedle(haystack_1)); // 3
+console.log(findNeedle(haystack_2)); // 5
+console.log(findNeedle(haystack_3)); // 30
+console.log(findNeedle(haystack_4)); // 5

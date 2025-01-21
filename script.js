94b8323cb1669c89729133d103561bcf5bda2a52
AlexDolz <aleksejdolznikov3@gmail.com>
2025-01-21 13:03:49 +0100

    updated

--- script.js
+++ script.js
@@ -1062,3 +1062,30 @@ function isTriangle(a, b, c) {
 console.log(isTriangle(1, 2, 2));
 console.log(isTriangle(7, 2, 2));
 console.log('*******************************');
+
+// ************************* Nr 40 *****************************
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+  const charCount = {};
+
+  if (string === '') return charCount;
+
+  for (let char of string) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+  return charCount;
+}
+
+console.log(count(''));
+console.log(count('a'));
+console.log(count('ab'));
+console.log(count('aba'));
+console.log(count('ABC'));

4794fda3bb5b608c7003ea07d78fb39b211a1a5e
AlexDolz <aleksejdolznikov3@gmail.com>
2025-01-23 13:54:56 +0100

    updated

--- script.js
+++ script.js
@@ -1165,3 +1165,16 @@ console.log(sumDigits(10));
 console.log(sumDigits(99));
 console.log(sumDigits(-32));
 console.log('****************************');
+
+// ***************************** Nr 45 ************************
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+function friend(friends) {
+  return friends.filter(friend => friend.length === 4);
+}
+
+console.log(friend(['Ryan', 'Kieran', 'Mark']));
+console.log(friend(['Ryan', 'Jimmy', '123', '4', 'Cool Man']));

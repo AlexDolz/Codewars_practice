25d4572dd09f5d35c09b1310ec0f7773cebf96ac
AlexDolz <aleksejdolznikov3@gmail.com>
2025-01-28 13:21:20 +0100

    updated

--- script.js
+++ script.js
@@ -1260,3 +1260,21 @@ function minValue(values) {
 console.log(minValue([1, 3, 1]));
 console.log(minValue([4, 7, 5, 7]));
 console.log('***********************************');
+
+// ************************* Nr 51 *****************************
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
+
+console.log(century(1705));
+console.log(century(1900));
+console.log(century(1601));
+console.log(century(2000));
+console.log(century(2742));

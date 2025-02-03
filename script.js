9c64f0d39f7f8c05d7aa65b80a2d019e9f5134bf
AlexDolz <aleksejdolznikov3@gmail.com>
2025-02-03 13:18:24 +0100

    updated

--- script.js
+++ script.js
@@ -1502,3 +1502,55 @@ function high(x) {
 console.log(high('man i need a taxi up to ubud'));
 console.log(high('what time are we climbing up the volcano'));
 console.log('**********************************');
+
+// ******************************** Nr 62 ***********************
+
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+function sumOfDifferences(arr) {
+  if (arr.length <= 1) return 0;
+  arr.sort((a, b) => b - a);
+  let sum = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    sum += arr[i] - arr[i + 1];
+  }
+  return sum;
+}
+
+console.log(sumOfDifferences([1, 2, 10]));
+console.log(sumOfDifferences([-3, -2, -1]));
+console.log('******************************');
+
+// *************************** Nr 63 ***************************
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+  return games.reduce((total, game) => {
+    const [x, y] = game.split(':').map(Number);
+    return total + (x > y ? 3 : x === y ? 1 : 0);
+  }, 0);
+}
+
+console.log(
+  points(['1:0', '2:0', '3:0', '4:0', '2:1', '3:1', '4:1', '3:2', '4:2', '4:3'])
+);
+console.log(
+  points(['1:1', '2:2', '3:3', '4:4', '2:2', '3:3', '4:4', '3:3', '4:4', '4:4'])
+);
+console.log('*******************');

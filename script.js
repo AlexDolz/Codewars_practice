db2633461cb9dd33adc1c712141e9dc1b17d3732
AlexDolz <aleksejdolznikov3@gmail.com>
2025-02-11 15:49:20 +0100

    +

--- script.js
+++ script.js
@@ -1988,3 +1988,30 @@ console.log(sameCase('a', 'D'));
 console.log(sameCase('\t', 'Z'));
 console.log(sameCase('H', ':'));
 console.log('***********************');
+// ************************* Nr 82 ***********************************
+
+// Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+  return str
+    .replace(/[^a-zA-Z]/g, '')
+    .split('')
+    .reverse()
+    .join('');
+}
+
+console.log(reverseLetter('krishan'));
+console.log(reverseLetter('ultr53o?n'));
+console.log(reverseLetter('ab23c'));

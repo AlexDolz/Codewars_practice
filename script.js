6d65333d29dc2c1f432a90d30c30b38bb7cb6689
AlexDolz <aleksejdolznikov3@gmail.com>
2025-02-11 11:40:19 +0100

    +

--- script.js
+++ script.js
@@ -1958,3 +1958,33 @@ console.log(operationResult(2, 3, 'power')); // 8
 console.log(operationResult(5, 3, 'min')); // 3
 console.log(operationResult(5, 3, 'max')); // 5
 console.log('************************************');
+
+// ***************************** Nr 81 *******************************
+
+// Write a function that will check if two given characters are the same case.
+
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
+
+function sameCase(a, b) {
+  if (!/[a-zA-Z]/.test(a) || !/[a-zA-Z]/.test(b)) {
+    return -1;
+  }
+  if (
+    (a === a.toLowerCase() && b === b.toLowerCase()) ||
+    (a === a.toUpperCase() && b === b.toUpperCase())
+  ) {
+    return 1;
+  }
+  return 0;
+}
+
+console.log(sameCase('C', 'B'));
+console.log(sameCase('b', 'a'));
+console.log(sameCase('d', 'd'));
+console.log(sameCase('A', 'd'));
+console.log(sameCase('a', 'D'));
+console.log(sameCase('\t', 'Z'));
+console.log(sameCase('H', ':'));
+console.log('***********************');
